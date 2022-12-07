9f47dc9b73013ed7e037ea10507420de05cbefd9
xandekk <alexandre.dosa@gmail.com>
2022-12-07 17:23:17 -0300

    add redirect if product empty

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :define_product
+  before_action :redirect_if_empty
+
   def show
-    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def define_product
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def redirect_if_empty
+    redirect_to root_path if @product.nil?
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include(product.price.to_s)
       expect(response.body).to include(product.desc)
     end
+
+    it "should redirect" do
+      get product_path("no_exist")
+      expect(response).to have_http_status(:redirect)
+    end
   end
 end
